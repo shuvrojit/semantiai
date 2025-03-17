f02f1ba7caaf68478862f07d1fd6a1e16c531f9c
Shuvrojit Biswas <jsuvro17@gmail.com>
2025-03-18 02:05:53 +0600

    fixup! Add ImportJobsModal component and integrate with CVPage

--- src/web/pages/CV.tsx
+++ src/web/pages/CV.tsx
@@ -1,20 +1,20 @@
 import ActionButton from "@/components/buttons/ActionButton";
 import ImportJobsModal from "@/components/modals/ImportJobsModal";
-import  { useState } from "react";
-const CVPage= () => {
- const [isImportModalOpen, setIsImportModalOpen] = useState(false);
+import { useState } from "react";
+const CVPage = () => {
+  const [isImportModalOpen, setIsImportModalOpen] = useState(false);
 
   return (
     <div>
       <h1>CV</h1>
-<ActionButton
-          onClick={() => setIsImportModalOpen(true)}
-        >
-          Import Jobs
-        </ActionButton>
-      <ImportJobsModal isOpen={isImportModalOpen} onClose={() => setIsImportModalOpen(false)} />
-  </div>
-
+      <ActionButton onClick={() => setIsImportModalOpen(true)}>
+        Import Jobs
+      </ActionButton>
+      <ImportJobsModal
+        isOpen={isImportModalOpen}
+        onClose={() => setIsImportModalOpen(false)}
+      />
+    </div>
   );
 };
 
